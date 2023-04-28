bb935da1a196d6fdafc17db5f8fbdfee8ea471eb
JuanK Valerio <jcvalerio@gmail.com>
2023-04-28 11:30:55 -0600

    feat(app): move from standard to semantic-release

---
+++ .releaserc.js
@@ -0,0 +1,41 @@
+const envCi = require('env-ci');
+
+const { branch, isCi, isPr, name, pr, prBranch, slug } = envCi();
+
+if (isCi) {
+  console.log(`Building repo ${slug} on ${name} service`);
+  if (isPr) {
+    console.log(
+      `Building Pull Request #${pr} originating from branch ${prBranch} and targeting branch ${branch}`
+    );
+  } else {
+    console.log(`Building branch ${branch}`);
+  }
+}
+
+const plugins = [
+  '@semantic-release/commit-analyzer',
+  '@semantic-release/release-notes-generator',
+  [
+    '@semantic-release/changelog',
+    {
+      changelogFile: 'CHANGELOG.md',
+    },
+  ],
+  [
+    '@semantic-release/git',
+    {
+      assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      message:
+        'chore(release): ${nextRelease.version} [skip-cd]\n\n${nextRelease.notes}',
+    },
+  ],
+];
+
+module.exports = {
+  branch: 'feature/poc-semantic-release',
+  branches: ['main', 'develop'],
+  plugins,
+  tagFormat: 'v${version}',
+  repositoryUrl: 'https://github.com/jcvalerio/semantic-versioning-poc',
+};
